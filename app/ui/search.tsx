eee8d7a646a9f55b237714e88373aabab43ec5a8
Ricardo Siqueira de Oliveira Leite <ricardooliveira88@gmail.com>
2023-11-12 15:52:19 -0300

    small improve

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,23 +4,22 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
-
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-const handleSearch = useDebouncedCallback((term) => {
-  console.log(`Searching... ${term}`);
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
 
-  const params = new URLSearchParams(searchParams);
-  if (term) {
-    params.set("query", term);
-  } else {
-    params.delete("query");
-  }
-  replace(`${pathname}?${params.toString()}`);
-}, 300);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
