f348e6e877a85c5b39434d3d055e18c825fc9c94
Ricardo Siqueira de Oliveira Leite <ricardooliveira88@gmail.com>
2023-11-12 15:29:29 -0300

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import '@/app/ui/global.css'
-import { inter } from '@/app/ui/fonts'
+import { Analytics } from "@vercel/analytics/react";
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
 export default function RootLayout({
   children,
@@ -13,6 +14,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
